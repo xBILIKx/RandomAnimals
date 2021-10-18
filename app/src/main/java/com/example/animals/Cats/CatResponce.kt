c9ffa037d9c7571ff43046ce027d2e35825a1075
bilik <xbilicx@gmail.com>
2021-10-18 21:20:54 +0300

    Delete comments

--- app/src/main/java/com/example/animals/Cats/CatResponce.kt
+++ app/src/main/java/com/example/animals/Cats/CatResponce.kt
@@ -3,17 +3,6 @@ package com.example.animals.Cats
 import com.google.gson.annotations.SerializedName
 
 data class CatResponce(
-    /*@SerializedName("id")
-    val id: Int,
-    @SerializedName("url")
-    val imgUrl: String,
-    @SerializedName("webpurl")
-    val webpurl: String,
-    @SerializedName("x")
-    val x: Double,
-    @SerializedName("y")
-    val y: Double*/
-
     @SerializedName("file")
     val imgUrl: String
 )
